2699a2373c4d63de74979b1928bf8511fcadec3c
DirtyCode <asasin.lol10@gmail.com>
2021-09-18 00:54:07 +0300

    Запис в таблицю

--- TestTask/TestTask.cpp
+++ TestTask/TestTask.cpp
@@ -30,7 +30,8 @@ int main()
     std::ifstream csv_reader("acme_worksheet.csv");
     std::string line;
     std::map<std::string, std::vector<DateAndHour>> employees;
-
+    std::vector<std::string> dateList;
+    dateList.resize(1);
 
     if (csv_reader.is_open()) 
     {
@@ -40,17 +41,43 @@ int main()
             ss.clear();
             std::cout << line << std::endl;
             ss = split(line, ",");
+            if (ss[1] != dateList.back()) 
+            {
+                dateList.push_back(ss[1]);
+            } 
             employees[ss[0]].push_back({ ss[1], ss[2] });
         }
     }
+    dateList.erase(dateList.begin(), dateList.begin() + 2);
 
     std::fstream fout;
     fout.open("reportcard.csv", std::ios::out);
     std::vector<std::string> vints;
     for (auto const& imap : employees)
         vints.push_back(imap.first);
-    std::vector<DateAndHour> date = employees[vints[0]];
+    fout << "Name/Date" << ",";
+    for (auto const& imap : dateList)
+        fout << imap << ","; 
+    fout << "\n";
+    for(auto const& imap : vints)
+    {
+        fout << imap << ",";
+        std::cout << imap;
+        std::vector<DateAndHour> date = employees[imap];
+        for (auto const& i : dateList)
+        {
+            for(auto const& j : date)
+            {
+                if ( i == j.date)
+                {
+                    fout << j.hour <<",";
+                }
+
+            }
+        }
+        fout << "\n";
+        std::cout << "\n";
+    }
     std::cout << "f";
-    //fout << employees
 }
 
