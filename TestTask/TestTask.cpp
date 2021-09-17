ae60490c1121bdead5e0b147f9c821d45685fd95
DirtyCode <asasin.lol10@gmail.com>
2021-09-17 19:09:58 +0300

    Start project

---
+++ TestTask/TestTask.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <fstream>
+#include<string>
+
+int main()
+{
+    std::ifstream csvReader("acme_worksheet.csv");
+    std::string line;
+    if (csvReader.is_open()) 
+    {
+        while (getline(csvReader, line))
+        {
+            std::cout << line << std::endl;
+        }
+    }
+}
